47a09af50c7c2f0f2b18dc1d4c80e4acdaade43c
sagar9836 <sagarpaal116@gmail.com>
2026-01-30 09:50:12 +0530

    made changes in the app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ const errorHandler = require("./middlewares/error.middleware");
 const app = express();
 
 app.use(express.json());   // MUST be here
-app.use("/tasks", taskRoutes);
+app.use("/task", taskRoutes);
 app.use(errorHandler);
 
+
+function logRequests(req, res, next) {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+app.use(logRequests);
+
+console.log("Task Manager API is running...");
 module.exports = app;
+ 
